1bcc481daced32dc2ec12eabb1071341b12ed8a9
Ajmal Nasumudeen <84835379+stormdotcom@users.noreply.github.com>
2024-11-13 18:17:51 +0530

    class

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("Hello, C#!");
+
+int a = 10;
+double b = 99.11;
+
+int myCastInt = (int) b;
+
+string firstName = "Ajmal";
+string lastName = "N";
+string fullName = $"My name is {firstName} {lastName}";
+
+Console.WriteLine(a);
+Console.WriteLine(myCastInt);
+Console.WriteLine(fullName[1]);
