3257c0c3d83a26baadaac353643dac560a0a31f7
Ajmal Nasumudeen <84835379+stormdotcom@users.noreply.github.com>
2024-11-13 18:16:55 +0530

    first

---
+++ AdvancedcS/AdvancedcS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AdvancedcS
+{
+   
+    public delegate void PrintMessage(string message);
+    class Program
+    {
+        struct Person
+        {
+            public string name;
+            public int marks;
+        }
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+       
+        static void Main(string[] args)
+        {
+            Person p1;
+            p1.name = "AJN";
+            p1.marks = 1;
+            int[] numbers = { 1, 2, 3,4,5,6,7,8,22,12,14,55,556 };
+            var evnNm = numbers.Where(n=> n%2 == 0);
+
+            foreach(var n in evnNm) { Console.WriteLine(n); }
+            PrintMessage p = new PrintMessage(DisplayMessage);
+            p("This is delegate example");
+        }
+    }
+}
